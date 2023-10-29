de1668e072403655880e68c82ed4e43e366edb41
JoelBaig <131587984+Joe482@users.noreply.github.com>
2023-10-29 15:59:05 +0100

    added JSON

---
+++ json.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+        "question": "Wie viele Planeten beherbergt unser Sonnensystem?",
+        "answer_1": "6",
+        "answer_2": "7",
+        "answer_3": "8",
+        "answer_4": "9",
+        "right_answer": "3"
+    },
+    {
+        "question": "Welcher Planet ist der kleinste in unserem Sonnensystem?",
+        "answer_1": "Merkur",
+        "answer_2": "Mars",
+        "answer_3": "Pluto",
+        "answer_4": "Venus",
+        "right_answer": "1"
+    },
+    {
+        "question": "Warum wurde Pluto der Planetenstatus aberkannt?",
+        "answer_1": "Er wurde von einem schwarzen Loch verschluckt",
+        "answer_2": "Er wurde von einem anderen Planeten aus der Umlaufbahn und dem Sonnensystem geworfen",
+        "answer_3": "Er wurde der Kategorie Zwergplanet zugeordnet",
+        "answer_4": "Er ist mit einem anderen Planeten kollidiert",
+        "right_answer": "3"
+    },
+    {
+        "question": "Welcher Planet ist der hellste am Nachthimmel?",
+        "answer_1": "Neptun",
+        "answer_2": "Uranus",
+        "answer_3": "Mars",
+        "answer_4": "Venus",
+        "right_answer": "4"
+    },
+    {
+        "question": "Welcher Planet unseres Sonnensystems hat die meisten Monde?",
+        "answer_1": "Jupiter",
+        "answer_2": "Saturn",
+        "answer_3": "Uranus",
+        "answer_4": "Merkur",
+        "right_answer": "2"
+    },
+    {
+        "question": "Welcher der folgenden Planeten hat Ringe?",
+        "answer_1": "Saturn",
+        "answer_2": "Neptun",
+        "answer_3": "Jupiter",
+        "answer_4": "Erde",
+        "right_answer": "1"
+    }
+]
